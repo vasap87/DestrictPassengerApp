eb8f23c07e6014c0a11106aa20814407e5305298
vasap87 <a200x@bk.ru>
2018-01-17 19:51:29 +0300

    change list item layout

--- app/src/main/java/ru/alexandrkotovfrombutovo/destrictpassengerapp/MainActivity.java
+++ app/src/main/java/ru/alexandrkotovfrombutovo/destrictpassengerapp/MainActivity.java
@@ -4,19 +4,15 @@ import android.app.Activity;
 import android.app.FragmentTransaction;
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.v4.view.MenuItemCompat;
 import android.util.Log;
 import android.view.Menu;
-import android.view.MenuItem;
-import android.widget.ShareActionProvider;
 
-import ru.alexandrkotovfrombutovo.destrictpassengerapp.fragments.TopFragment;
+import ru.alexandrkotovfrombutovo.destrictpassengerapp.fragments.RouteListFragment;
 
 
 public class MainActivity extends Activity {
 
     private static final String TAG = "MainActivity";
-    private ShareActionProvider shareActionProvider;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,7 +21,7 @@ public class MainActivity extends Activity {
         setContentView(R.layout.activity_main);
 
         FragmentTransaction transaction = getFragmentManager().beginTransaction();
-        TopFragment top = new TopFragment();
+        RouteListFragment top = new RouteListFragment();
         transaction.add(R.id.content_frame, top);
         transaction.commit();
 
@@ -34,15 +30,10 @@ public class MainActivity extends Activity {
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         getMenuInflater().inflate(R.menu.menu_main, menu);
-        MenuItem shareItem = menu.findItem(R.id.action_share);
-        shareActionProvider = (ShareActionProvider)shareItem.getActionProvider();
+
         return true;
     }
 
-
-    public void doShare(Intent intent){
-        shareActionProvider.setShareIntent(intent);
-    }
 
     @Override
     protected void onSaveInstanceState(Bundle outState) {
